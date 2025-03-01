e7a5f8a2f431fa73617242cc496a03a91c3a5fa9
Khanh Hua <giakhanh2487@gmail.com>
2025-03-01 23:06:08 +0100

    task2: allow configuration via function parameter

---
+++ csv-parser/app-v3/Main.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Control.Monad (void, forM)
+import qualified Data.Text as T
+
+data CsvType = Text | Number
+data CsvData = CsvText String | CsvNumber Int
+  deriving Show
+
+rawInputData = "10\n20\n30"
+
+main :: IO ()
+main = do
+  let rows = T.splitOn "\n" rawInputData
+      nums = map (extractData Number . T.unpack) rows
+
+  forM nums (\num -> putStrLn $ show num)
+  return ()
+
+extractData csvType s =
+  case csvType of
+    Text -> CsvText s
+    Number -> CsvNumber $ extractNumber s
+
+extractNumber s = read s :: Int
