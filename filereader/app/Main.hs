a944e83f8f8b2434ea918b916cbc7b06947c3108
Khanh Hua <giakhanh2487@gmail.com>
2025-04-13 18:10:40 +0200

    Data modeling

--- filereader/app/Main.hs
+++ filereader/app/Main.hs
@@ -1,9 +1,34 @@
 module Main where
 
+import qualified Data.Char as Char
 import System.IO (IOMode(..), openFile, hGetContents)
+
+split  :: String -> [String]
+split s = case w of
+    "" -> []
+    _ -> w : split (dropComma s'')
+ where
+  (w, s'') = break isComma s
+  isComma c =
+    c == ','
+  dropComma = dropWhile isComma
 
 main :: IO ()
 main = do
     handle <- openFile "dates.csv" ReadMode
     str <- hGetContents handle
-    putStrLn str
+    let (header:body) = lines str 
+        (tomLine:rest) = body
+        (jerryLine:[]) = rest
+    print tomLine
+    print jerryLine
+  
+    let [name, dob, origin] = split jerryLine
+        figure = Figure name dob origin
+    print figure
+  --  putStrLn $ show figure
+
+data Figure = Figure String String String
+
+instance Show Figure where
+  show (Figure name _ origin) = name <> " (" <> origin <> ")"
